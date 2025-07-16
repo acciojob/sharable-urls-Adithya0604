f75730d5fb63a0a7b419c8dc253d9e4d484166fc
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-07-16 22:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+const changeURL = document.getElementById('url');
+const GraduatedYear = document.getElementById('year');
+const Name = document.getElementById('name');
+const URL = 'https://localhost:8080/?';
+
+
+document.getElementbyId('button').addEventListener('click', () => {
+	const passName = Name.value.trim();
+	const passYear = GraduatedYear.value.trim();
+
+	let queryArray = [];
+
+	if(passName){
+		queryArray.push(`name=${encodeURIComponent(passName)}`)
+	}
+	if(passYear){
+		queryArray.push(`year=${encodeURIComponent(passYear)}`)
+	}
+
+	if(queryArray.length > 0){
+		changeURL.textContent = `${URL}${queryArray.join('&')}`;
+	}else{
+	changeURL.textContent = `Please Enter The Details`
+		}
+})
